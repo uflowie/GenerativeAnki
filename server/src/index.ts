506721e71438761ce824fe2f97dd605bbac4948f
uflowie <bananasawsm@gmail.com>
2025-03-20 22:54:08 +0100

    add cards endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
-import { generateText } from "ai"
-import { createGoogleGenerativeAI, google } from "@ai-sdk/google"
+import { generateObject, generateText } from "ai"
+import { createGoogleGenerativeAI } from "@ai-sdk/google"
+import { z } from 'zod'
+import { logger } from 'hono/logger'
+import { zValidator } from '@hono/zod-validator'
 
 type Bindings = {
   GOOGLE_API_KEY: string
@@ -8,14 +11,40 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
+app.use(logger())
+
 app.get('/', async (c) => {
+  const model = createGoogleGenerativeAI({ apiKey: c.env.GOOGLE_API_KEY }).languageModel("gemini-2.0-flash-thinking-exp-01-21")
+
   const random = Math.random() * 1000000000;
   console.log(random);
   const { text } = await generateText({
-    model: createGoogleGenerativeAI({ apiKey: c.env.GOOGLE_API_KEY }).languageModel("gemini-2.0-flash-thinking-exp-01-21"),
+    model,
     prompt: "Tell me something random, here is some inspiration: " + random,
   })
   return c.text(text)
+})
+
+app.post('/cards', zValidator('json', z.object({
+  topic: z.string().min(1),
+})), async (c) => {
+  const { topic } = c.req.valid('json')
+
+  const model = createGoogleGenerativeAI({ apiKey: c.env.GOOGLE_API_KEY }).languageModel("gemini-2.0-flash-exp")
+
+  const { object } = await generateObject({
+    model,
+    schema: z.object({
+      topic: z.string(),
+      cards: z.array(z.object({
+        question: z.string(),
+        answer: z.string(),
+      })),
+    }),
+    temperature: 1,
+    prompt: `Generate a set of flashcards for the topic: ${topic}. Find a suitable name for the flashcards.`,
+  });
+  return c.json(object)
 })
 
 export default app
